d6e1c84fa811339aec44c6d266ccfea2984df052
sanctificed <gustavo28bt@gmail.com>
2026-05-22 17:26:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import random
 
+
+elements = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+senhacarct = int(input("Insira a quantidade de caracteres: "))
+
+senha = ""
+
+for i in range (senhacarct):
+senha += random.choice(elements)
+
+print("Essa é sua senha: " + senha

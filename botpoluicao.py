a4e752cdffa210fa31cde6bf1bddbe885a523cc4
sanctificed <gustavo28bt@gmail.com>
2026-07-10 16:21:04 -0300

    Delete botpoluicao.py

--- botpoluicao.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
-
-@bot.event
-async def on_ready():
-    print(f'Fizemos login como {bot.user}')
